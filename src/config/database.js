037608acdac08df0d6a1e205bbb696bc595f981f
dvaJi <francisco_p@outlook.com>
2019-06-15 19:52:58 -0400

    chore(ci): improve circleci config

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: 'root',
-    password: null,
+    password: 'my-secret-pw',
     database: 'reivens',
     host: '127.0.0.1',
     dialect: 'mysql'
